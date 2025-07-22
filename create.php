5fdd190ecc04b57e107fa14b6a9e61077aea0ace
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2025-07-22 16:27:04 -0300

    Create create.php
    Arquivo para criar e adicionar um texto no banco de dados

---
+++ create.php
@@ -0,0 +1,23 @@
+<?php include(db.php); 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $titulo = $_POST["titulo"];
+    $conteudo = $_POST["conteudo"];
+
+    $sql = "INSERT INTO textos (titulo, conteudo) VALUES ('$titulo', '$conteudo')";
+    if($conn->query($sql) === true){
+        echo "Txto adicionado com sucesso!";
+    }else{
+        echo "Erro: " . $conn->error;
+    }
+
+}
+?>
+<form method="POST">
+  
+    <h2>Adicionar texto do curso de inglês</h2>
+    <input type="text" name="titulo" placeholder="Título" required>
+    <textarea name="conteudo" placeholder="Comteúdo do texto" rows="5" cols="40" required></textarea> <br><br>
+    <button type="submit">Salvar</button>
+
+</form>
