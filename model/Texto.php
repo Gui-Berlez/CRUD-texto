d77e6383d902e960c6d1f9d8da10461c33a6c223
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2025-07-22 21:17:44 -0300

    Update Textos.php
    Finalização das lógicas do banco de dados. Para a integração do padrão MVC

---
+++ model/Texto.php
@@ -0,0 +1,35 @@
+<?php
+require_once("db.php");
+
+Class Texto{
+    public static function listarTodos($conn){
+        $sql = "SELECT * FROM textos ORDER BY data_criacao DESC";
+        return $conn->quary($sql);
+    }
+
+    public static function buscarPorId($conn, $id){
+        $stmt = $conn->prepare("SELECT * FROM textos WHERE id = ?");
+        $stmt->bind_param("i",$id);
+        $stmt->execute();
+        return $stt->get_result()->fetch_assoc();
+    }
+
+    public static function inserir($conn, $titulo, $conteudo){
+        $stmt = $conn->prepare("INSERT  INTO textos(titulo, conteudo) VALUES(?, ?)");
+        $stmt->bibd_param("ss",$titulo, $conteudo);
+        return $stmt->execute();
+    }
+
+    public static function atualizar($conn, $id, $titulo, $conteudo){
+        $stmt = $conn->prepare("UPDATE textos SET titulo = ?, conteudo = ? WHERE id = ?");
+        $stmt->bind_param("ssi", $titulo, $conteudo, $id);
+        return $stmt->execute();
+    }
+
+    public static function excluir($conn, $id){
+        $stmt = $conn->prepare("DELETE FROM textos WHERE id = ?");
+        $stmt->bind_param("i",$id);
+        return $stmt->execute();
+    }
+
+}
