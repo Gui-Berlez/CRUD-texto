5d876a6feeb8d49e8cf755490c6fc5c144b32707
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2025-07-23 22:50:55 -0300

    CSS create.php
    inicio do css

--- view/read.php
+++ view/read.php
@@ -1,18 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de textos</title>
     <link rel="stylesheet" href="../css/read.css">
 </head>
-<div class="header">
-<h1>Lista de textos</h1>
-<a class="newText" href="../view/create.php">Adicionar novo texto</a>
-</div>
-<?php while ($linha = $resultados->fetch_assoc()) : ?>
-    <div class="card">
-        <h3><?= htmlspecialchars($linha["titulo"]) ?></h3>
-        <p><?= nl2br(htmlspecialchars($linha["conteudo"])) ?></p>
-        <small><?= $linha["data_criacao"] ?></small><br>
-        <a href="../controller/TextosController.php?acao=editar&id=<?= $linha["id"] ?>">Editar</a> |
-        <a href="../controller/TextosController.php?acao=excluir&id=<?= $linha["id"] ?>" onclick="return confirm('Deseja excluir?')">Excluir</a>
+<body>
+    <div class="header">
+    <h1>Lista de textos</h1>
+    <a class="newText" href="../view/create.php">Adicionar novo texto</a>
     </div>
-<?php endwhile; ?>
+    <?php while ($linha = $resultados->fetch_assoc()) : ?>
+        <div class="card">
+            <h3><?= htmlspecialchars($linha["titulo"]) ?></h3>
+            <p><?= nl2br(htmlspecialchars($linha["conteudo"])) ?></p>
+            <small><?= $linha["data_criacao"] ?></small><br>
+            <a href="../controller/TextosController.php?acao=editar&id=<?= $linha["id"] ?>">Editar</a> |
+            <a href="../controller/TextosController.php?acao=excluir&id=<?= $linha["id"] ?>" onclick="return confirm('Deseja excluir?')">Excluir</a>
+        </div>
+    <?php endwhile; ?>
+</body>
+</html>
+
--- view/update.php
+++ view/update.php
@@ -1,12 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar texto</title>
     <link rel="stylesheet" href="../css/update.css">
 </head>
-<h2>Editar texto</h2>
-<form class="form-container" method="POST" action="../controller/TextosController.php?acao=editar&id=<?= $texto["id"] ?>">
-    <input type="text" name="titulo" value="<?= htmlspecialchars($texto["titulo"]) ?>" required><br><br>
-    <textarea name="conteudo" rows="5" cols="40" required><?= htmlspecialchars($texto["conteudo"]) ?></textarea><br><br>
-    <button type="submit">Atualizar</button>
-</form>
-<a href="../controller/TextosController.php">Voltar</a>
+<body>
+ <div class="form-container">
+     <h2>Editar texto</h2>
+     <form method="POST" action="../controller/TextosController.php?acao=editar&id=<?= $texto["id"] ?>">
+         <input type="text" name="titulo" value="<?= htmlspecialchars($texto["titulo"]) ?>" required><br><br>
+         <textarea name="conteudo" rows="5" cols="40" required><?= htmlspecialchars($texto["conteudo"]) ?></textarea><br><br>
+         <button type="submit">Atualizar</button>
+     </form>
+     <a href="../controller/TextosController.php">Voltar</a>
+ </div>
+</body>
+</html>
+
+
