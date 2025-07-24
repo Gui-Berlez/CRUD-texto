d04b2f48c797db4ffbb759bea3090a7c6f5d03b8
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2025-07-23 23:12:03 -0300

    CSS create.php
    Finalizado!

--- view/create.php
+++ view/create.php
@@ -1,6 +1,20 @@
-<h2>Adicionar texto do curso de inglês</h2>
-<form method="POST" action="../controller/TextosController.php?acao=criar">
-    <input type="text" name="titulo" placeholder="Título" required><br><br>
-    <textarea name="conteudo" placeholder="Conteúdo do texto" rows="5" cols="40" required></textarea> <br><br>
-    <button type="submit">Salvar</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/create.css">
+</head>
+<body>
+    <div class="create-container">
+        <h2>Adicionar novo texto</h2>
+        <form method="POST" action="../controller/TextosController.php?acao=criar">
+            <input type="text" name="titulo" placeholder="Título" required><br><br>
+            <textarea name="conteudo" placeholder="Conteúdo do texto" rows="5" cols="40" required></textarea> <br><br>
+            <button type="submit">Salvar</button>
+        </form>
+        <a href="../controller/TextosController.php">Voltar</a>
+    </div>
+</body>
+</html>
