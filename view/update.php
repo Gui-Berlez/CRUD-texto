89396de039a0ed69ece5dc4f409d9a09d7acf9b2
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2025-07-23 22:11:43 -0300

    CSS update.php

--- view/update.php
+++ view/update.php
@@ -1,7 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Editar texto</title>
+    <link rel="stylesheet" href="../css/update.css">
+</head>
 <h2>Editar texto</h2>
-<form method="POST" action="../controller/TextosController.php?acao=editar&id=<?= $texto["id"] ?>">
+<form class="form-container" method="POST" action="../controller/TextosController.php?acao=editar&id=<?= $texto["id"] ?>">
     <input type="text" name="titulo" value="<?= htmlspecialchars($texto["titulo"]) ?>" required><br><br>
     <textarea name="conteudo" rows="5" cols="40" required><?= htmlspecialchars($texto["conteudo"]) ?></textarea><br><br>
     <button type="submit">Atualizar</button>
 </form>
-<a href="../controller/TextosController.php">← Voltar</a>
+<a href="../controller/TextosController.php">Voltar</a>
